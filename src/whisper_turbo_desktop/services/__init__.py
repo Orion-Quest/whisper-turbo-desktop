1984833874abcccdf3e6146fcfa6bf8f730348cf
JunHeng Ye <qwe13867065897@gmail.com>
2026-04-05 13:00:28 +0800

    Add src/whisper_turbo_desktop/services/__init__.py

---
+++ src/whisper_turbo_desktop/services/__init__.py
@@ -0,0 +1,11 @@
+from whisper_turbo_desktop.services.diagnostics_service import DiagnosticItem, DiagnosticsService
+from whisper_turbo_desktop.services.history_service import HistoryService
+from whisper_turbo_desktop.services.settings_service import AppSettings, SettingsService
+
+__all__ = [
+    "AppSettings",
+    "DiagnosticItem",
+    "DiagnosticsService",
+    "HistoryService",
+    "SettingsService",
+]
