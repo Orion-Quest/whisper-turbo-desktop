b369f0bc8dd2d240d6f65a2d9a08794ecdbace86
JunHeng Ye <qwe13867065897@gmail.com>
2026-04-05 12:59:36 +0800

    Add src/whisper_turbo_desktop/models/__init__.py

---
+++ src/whisper_turbo_desktop/models/__init__.py
@@ -0,0 +1,5 @@
+from whisper_turbo_desktop.models.history import HistoryRecord
+from whisper_turbo_desktop.models.queue_task import QueueTask
+from whisper_turbo_desktop.models.transcription import TranscriptionRequest
+
+__all__ = ["HistoryRecord", "QueueTask", "TranscriptionRequest"]
